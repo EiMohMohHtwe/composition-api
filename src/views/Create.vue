378ad98fc19cb3efda9b85012b0b3045d16391cc
EiMohMohHtwe <eimohmohhtwe@gmail.com>
2021-10-11 11:52:24 +0100

    create post and save to firestore

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -16,6 +16,7 @@
 <script>
 import {ref} from 'vue'
 import {useRouter} from 'vue-router'
+import {db} from "../firebase/config"
 export default {
     setup(){
         let router=useRouter();
@@ -31,19 +32,28 @@ export default {
             tag.value=""
         }
         let addPost=async()=>{
-           await fetch("http://localhost:3000/posts",{
-               method:"POST",
-               headers:{
-                   "Content-type":"application/json"
-               },
-               body:JSON.stringify(
-                   {
-                       title:title.value,
-                       body:body.value,
-                       tags:tags.value,
-                   }
-               )
-           })
+          let newPost={
+            title:title.value,
+            body:body.value,
+            tags:tags.value,
+          }
+
+          let res=await db.collection("posts").add(newPost)
+
+          //await fetch("http://localhost:3000/posts",{
+          //    method:"POST",
+          //    headers:{
+          //        "Content-type":"application/json"
+          //    },
+          //    body:JSON.stringify(
+          //        {
+          //            title:title.value,
+          //            body:body.value,
+          //            tags:tags.value,
+          //        }
+          //    )
+          //})
+          
            router.push("/");
         }
 
